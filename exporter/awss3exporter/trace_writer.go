18031791113115fab6383dd9971a596561c3acbd
Przemek Delewski <pdelewski@sumologic.com>
2022-05-06 13:27:16 +0200

    extracting common implementation of uploading buffer into s3

--- exporter/awss3exporter/trace_writer.go
+++ exporter/awss3exporter/trace_writer.go
@@ -14,33 +14,6 @@
 
 package awss3exporter
 
-import (
-	"bytes"
-	"fmt"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-)
-
 func pushTracesData(e *S3Exporter, buf []byte, region string, bucket string, filename string) error {
-	// create a reader from data data in memory
-	reader := bytes.NewReader(buf)
-
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(region)},
-	)
-	uploader := s3manager.NewUploader(sess)
-
-	_, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(filename),
-		Body:   reader,
-	})
-	if err != nil {
-		fmt.Printf("Unable to upload %q to %q, %v", filename, bucket, err)
-		e.logger.Error("Uploading to S3")
-	}
-
-	return nil
+	return pushOtlpJson(e, buf, region, bucket, filename)
 }
